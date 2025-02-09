3f174ade85e43cf4139ba3f39d6c8a24a5d72efd
Artemidus <zhuravlev89artem@gmail.com>
2025-02-09 22:31:29 +0400

    Сделано ДЗ

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String lastName;
@@ -14,9 +16,29 @@ public class Author {
     public String getLastName() {
         return lastName;
     }
-
+    // метод преобразования класса в строку
     @Override
     public String toString() {
         return firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals (Object other) {
+        // проверка, что оба объекта ссылаются на один объект
+        if (this == other) {
+            return true;
+        }
+        // проверка на разность классов и на null
+        if (other == null || this.getClass() != other.getClass()) {
+            return false;
+        }
+        // приведение типа
+        Author a = (Author) other;
+        return Objects.equals(firstName, a.firstName) && Objects.equals(lastName, a.lastName);
+    }
+    // генерация хэш-кода на основе полей
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(firstName, lastName);
     }
 }
